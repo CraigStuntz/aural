f7f42251b8b3d210f09fde1732e279f53b209b77
Craig Stuntz <CraigStuntz@users.noreply.github.com>
2023-12-16 13:39:25 -0500

    Better centralization of sorting

--- Sources/AudioUnitComponents.swift
+++ Sources/AudioUnitComponents.swift
@@ -5,7 +5,8 @@ struct AudioUnitComponents {
   static func components(filter: Filter?) -> [AVAudioUnitComponent] {
     if filter == nil {
       let componentDescription = AudioComponentDescription()
-      return AVAudioUnitComponentManager.shared().components(matching: componentDescription)
+      return AVAudioUnitComponentManager.shared().components(matching: componentDescription).sorted(
+        by: AudioUnitComponents.isLessThan)
     }
     return AVAudioUnitComponentManager.shared().components(passingTest: { (component, _) in
       switch filter!.filterType {
@@ -16,6 +17,15 @@ struct AudioUnitComponents {
       case .type:
         return component.typeName == filter!.name
       }
-    })
+    }).sorted(by: AudioUnitComponents.isLessThan)
+  }
+
+  static func isLessThan(_ c1: AVAudioUnitComponent, _ c2: AVAudioUnitComponent) -> Bool {
+    return (c1.manufacturerName.caseInsensitiveCompare(c2.manufacturerName) == .orderedAscending)
+      || (c1.manufacturerName.caseInsensitiveCompare(c2.manufacturerName) == .orderedSame
+        && c1.name.caseInsensitiveCompare(c2.name) == .orderedAscending)
+      || (c1.manufacturerName.caseInsensitiveCompare(c2.manufacturerName) == .orderedSame
+        && c1.name.caseInsensitiveCompare(c2.name) == .orderedSame
+        && c1.typeName.caseInsensitiveCompare(c2.typeName) == .orderedAscending)
   }
 }
